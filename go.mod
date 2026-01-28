a0c138abba73e570170bdfab1d8668d86bd38468
Jake Goritski <jgoritski@gmail.com>
2026-01-28 06:52:27 -0800

    get exif orientation

--- go.mod
+++ go.mod
@@ -2,4 +2,18 @@ module earthmanlabs/gthumb
 
 go 1.25.5
 
-require github.com/h2non/bimg v1.1.9 // indirect
+require (
+	github.com/anthonynsimon/bild v0.14.0 // indirect
+	github.com/dsoprea/go-exif/v3 v3.0.1 // indirect
+	github.com/dsoprea/go-logging v0.0.0-20200710184922-b02d349568dd // indirect
+	github.com/dsoprea/go-utility/v2 v2.0.0-20221003172846-a3e1774ef349 // indirect
+	github.com/go-errors/errors v1.4.2 // indirect
+	github.com/golang/geo v0.0.0-20210211234256-740aa86cb551 // indirect
+	github.com/h2non/bimg v1.1.9 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/spf13/cobra v0.0.5 // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	golang.org/x/image v0.18.0 // indirect
+	golang.org/x/net v0.0.0-20221002022538-bcab6841153b // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,59 @@
 package main
 
 import (
-	"github.com/h2non/bimg"
+	"fmt"
+
+	"github.com/anthonynsimon/bild/imgio"
+	"github.com/anthonynsimon/bild/transform"
+	exif "github.com/dsoprea/go-exif/v3"
+	exifcommon "github.com/dsoprea/go-exif/v3/common"
+	log "github.com/dsoprea/go-logging"
 )
 
 func main() {
-	buffer, err := bimg.Read("examples/image.jpg")
-	if err != nil {
+	buffer, err := imgio.Open("examples/image.jpg")
+	log.PanicIf(err)
+	bounds := buffer.Bounds().Max
+	thumbnail := transform.Resize(buffer, bounds.X/4, bounds.Y/4, transform.Linear)
+	if err := imgio.Save("examples/thumb.jpg", thumbnail, imgio.JPEGEncoder(100)); err != nil {
 		panic(err)
 	}
-	newImage, err := bimg.NewImage(buffer).Thumbnail(300)
-	bimg.Write("examples/thumb.jpg", newImage)
+	rawExif, err := exif.SearchFileAndExtractExif("examples/image.jpg")
+	im, err := exifcommon.NewIfdMappingWithStandard()
+	log.PanicIf(err)
+	ti := exif.NewTagIndex()
+	_, index, err := exif.Collect(im, ti, rawExif)
+	log.PanicIf(err)
+	tagName := "Orientation"
+	rootIfd := index.RootIfd
+	results, err := rootIfd.FindTagWithName(tagName)
+	if len(results) < 1 {
+		panic("Failed to find 'Orientation' in EXIF!")
+	}
+	ite := results[0]
+
+	valueRaw, err := ite.Value()
+	log.PanicIf(err)
+	// EXIF Orientations
+	// 1 = Horizontal (normal)
+	// 2 = Mirror horizontal
+	// 3 = Rotate 180
+	// 4 = Mirror vertical
+	// 5 = Mirror horizontal and rotate 270 CW
+	// 6 = Rotate 90 CW
+	// 7 = Mirror horizontal and rotate 90 CW
+	// 8 = Rotate 270 CW
+	value := valueRaw.([]uint16)[0]
+	switch value {
+	case 1:
+		fmt.Println(value, "Normal")
+	case 2:
+		fmt.Println(value, "Mirror horizontal")
+	case 3:
+		fmt.Println(value, "Rotate 180")
+	case 4:
+		fmt.Println(value, "Mirror vertical")
+	default:
+		fmt.Println(value, "TODO")
+	}
 }
